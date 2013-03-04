913d0a4b09e95d2bae14654b5c2761220ff4491b
Shayanmont <shayanm@gwu.edu>
2013-03-04 17:54:47 -0500

    require statement added

--- lib/personal_favicon_plugin.rb
+++ lib/personal_favicon_plugin.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'net/https'
+require 'uri'
 require 'personal_favicon_plugin/favicon'
 
 module PersonalFaviconPlugin
--- lib/personal_favicon_plugin/favicon.rb
+++ lib/personal_favicon_plugin/favicon.rb
@@ -1,7 +1,3 @@
-require 'net/http'
-require 'net/https'
-require 'uri'
-
 class Favicon
 
 	#attr_accessor :@main_url
